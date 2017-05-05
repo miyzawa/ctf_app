86b16af80793db6819e7c48d40e6a0d1eccc471a
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-05-05 16:08:19 +0200

    ルーディング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#index"
+  get "/help" => "static_pages#help"
+  get "/about" => "static_pages#about"
+  resources :users
+
+  namespace :admin do
+    root "static_pages#index", as: :admin
+    resources :question
+  end
 end
