11b471bb4a41759d2aa50d54821020b81152417a
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-05-07 08:19:11 +0200

    ranking画面のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#index"
   get "/help" => "static_pages#help"
   get "/about" => "static_pages#about"
+  get "/ranking" => "user#index"
   devise_for :users
   resources :questions, only: [:index, :show]
   namespace :admin do
