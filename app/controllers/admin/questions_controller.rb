523504a58e6586d03a3d13ce13013e625569a0f4
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-05-07 16:44:41 +0200

    問題を削除できるように

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,5 +1,7 @@
 class Admin::QuestionsController < ApplicationController
+  before_action :set_question, only: %i[edit, update, destroy]
   def index
+    @questions = Question.all
   end
 
   def show
@@ -19,12 +21,19 @@ class Admin::QuestionsController < ApplicationController
   end
 
   def destroy
+    @question.destroy
+    redirect_to admin_questions_path
   end
 
   def update
   end
 
-  def user_params
-    params.require(:question).permit(:subject, :context, :point, :answer)
-  end
+  private
+    def user_params
+      params.require(:question).permit(:subject, :context, :point, :answer)
+    end
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
 end
