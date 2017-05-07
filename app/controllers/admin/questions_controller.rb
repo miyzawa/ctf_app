c09ae4f1931c8a7812ec5b8c47454bf3af447482
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-05-07 14:21:12 +0200

    アンケートが作成できます（空でも）

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -6,14 +6,26 @@ class Admin::QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(user_params)
+    if @question.save
+      redirect_to admin_questions_path, notice: "問題を作成しました"
+    else
+      flash.now[:alert] = "問題作成に失敗しました"
+      render :new
+    end
   end
 
   def destroy
   end
 
   def update
+  end
+
+  def user_params
+    params.require(:question).permit(:subject, :context, :point, :answer)
   end
 end
