a80bfe7f4a2940dcbcb24f9631b39f3f42da53aa
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-05-07 06:16:40 +0200

    rails ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "static_pages#index"
   get "/help" => "static_pages#help"
   get "/about" => "static_pages#about"
-  resources :users
-
+  devise_for :users
+  resources :questions, only: [:index, :show]
   namespace :admin do
     root "static_pages#index", as: :admin
-    resources :question
+    resources :questions
   end
 end
